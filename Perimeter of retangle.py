981d5c6605335d9408e3114b445a2cdae86058e8
Riinaaal <Riinaaal@metropolia.fi>
2023-08-25 11:35:58 +0300

    Initial commit

---
+++ Perimeter of retangle.py
@@ -0,0 +1,7 @@
+length = int(input("Enter length of rectangle: "))
+width = int(input("Enter width of rectangle: "))
+
+perimeter = length * 2 + width * 2
+areaOfRectangle = length * width
+print("perimeter of rectangle is: ", perimeter)
+print("Area of rectangle is: ", areaOfRectangle)
