9b245de80e63fc14074fac7e4d9d246f103f3616
Riinaaal <Riinaaal@metropolia.fi>
2023-08-30 15:05:45 +0300

    Initial commit

--- Radius of circle.py
+++
@@ -1,6 +0,0 @@
-import math
-
-radius = float(input("Enter the radius: "))
-areaCircle = math.pi *radius ** 2
-print("Area of the circle is {:.2f}".format(areaCircle))
-
--- Random number lock.py
+++
@@ -1,8 +0,0 @@
-import random
-
-randomList3 = random.sample(range(0, 9), 3)
-print(randomList3)
-
-randomList4 = random.sample(range(1, 6), 4)
-print(randomList4)
-
