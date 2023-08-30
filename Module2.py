b7732e49a1329c1247fb34a8679f7f1183984f43
Riinaaal <Riinaaal@metropolia.fi>
2023-08-30 15:02:41 +0300

    Initial commit

--- Module2.py
+++ Module2.py
@@ -1,6 +1,7 @@
 # Module 2 assignment 1
 name = input("Enter your name: ")
 print("Hello ", name,"!")
+print()
 
 
 # Module 2 assignment 2
@@ -9,6 +10,7 @@ import math
 radius = float(input("Enter the radius: "))
 areaCircle = math.pi *radius ** 2
 print("Area of the circle is {:.2f}".format(areaCircle))
+print()
 
 
 # Module 2 assignment 3
@@ -19,6 +21,7 @@ perimeter = length * 2 + width * 2
 areaOfRectangle = length * width
 print("perimeter of rectangle is: ", perimeter)
 print("Area of rectangle is: ", areaOfRectangle)
+print()
 
 
 # Module 2 assignment 4
@@ -31,6 +34,7 @@ product = num1 * num2 * num3
 average = (num1 + num2 + num3) / 3
 
 print("Sum of the numbers is ", sum, "product of the numbers is ", product, "average of the numbers is ", average)
+print()
 
 
 # Module 2 assignment 5
@@ -50,13 +54,14 @@ rndKilogram = int(kilogram)
 grams = float(kilogram - rndKilogram) * 1000
 
 print("The weight in modern units: \n", rndKilogram, "kilograms and {:.2f}".format(grams), "grams.")
+print()
 
 
 # Module 2 assignment 6
 import random
 
 randomList3 = random.sample(range(0, 9), 3)
-print(randomList3)
+print("Your 3-digit code is: ", randomList3)
 
 randomList4 = random.sample(range(1, 6), 4)
-print(randomList4)
+print("Your 4-digit code is : ", randomList4)
