ed600e5be69ea6af91dfd8ed38a251e43668665a
Riinaaal <Riinaaal@metropolia.fi>
2023-09-07 18:01:33 +0300

    Initial commit

--- Module3.py
+++ Module3.py
@@ -45,7 +45,7 @@ print()
 
 year = int(input("Enter a year: "))
 
-if year % 4 == 0 or (year % 100 == 0 and year % 400 == 0):
+if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
     print("It is a leap year")
 else:
     print("It is not a leap year")
